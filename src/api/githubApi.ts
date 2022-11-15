4a0999dff0e3bf8d28bec1e45cfcfdee5deb41c4
adxell <adxell02@gmail.com>
2022-11-15 18:00:35 -0500

    first commit

---
+++ src/api/githubApi.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+
+export const githubApi = axios.create({
+    baseURL: 'https://api.github.com/repos/facebook/react',
+    headers: {
+        Authorization: 'Bearer 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'
+    }
+})
